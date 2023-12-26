75afef7e7aa5da5de12ffe1daabeae9a92ffd5c0
HeartLezz7 <heartchantapath@gmail.com>
2023-12-26 14:04:10 +0700

    1st draft use effect lab

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
 
 function App() {
+  console.log("App"); //1.
+
+  useEffect(() => console.log("Effect"), []);
+
+  //2.
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          onChange={console.log("IMG")} //1.
+        />
+        <div className="count">
+          <button>+</button>
+          {/* <p>{3.}</p>  */}
+          <button>-</button>
+        </div>
       </header>
     </div>
   );
